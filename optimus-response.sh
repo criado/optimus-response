cbfb5aa77508879bd936363158f98ad2694e4a11
Criado <criado@math.tu-berlin.de>
2019-10-18 20:42:34 +0200

    first step out of four

---
+++ optimus-response.sh
@@ -0,0 +1,11 @@
+# When called, start listening
+
+# When receiving a signal, stop the listening
+
+# Plug the prompt into guille's program
+output="Nani the fuck did you say about me, you little shit"
+
+# Get the output into the current active window
+#current_window= `xdotool getwindowfocus getwindowname`
+
+xdotool sleep 3 type "${output}"
